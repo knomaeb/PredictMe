6bc8e5213803842710b62364e29723221fb57eeb
Sunil Rana <sunil.rana320778@gmail.com>
2025-06-14 10:27:13 +0530

    feat: Update NetworkService and AppModule

--- app/src/main/java/com/example/predictme/di/AppModule.kt
+++ app/src/main/java/com/example/predictme/di/AppModule.kt
@@ -1,7 +1,9 @@
 package com.example.predictme.di
 
 import android.content.Context
-import com.example.predictme.data.network.NetworkService
+import com.example.predictme.data.network.NetworkServiceForAge
+import com.example.predictme.data.network.NetworkServiceForGender
+import com.example.predictme.data.network.NetworkServiceForNationality
 import com.example.predictme.utils.network.NetworkConnected
 import com.example.predictme.utils.network.NetworkHelper
 import dagger.Module
@@ -15,20 +17,52 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
+
+    private const val BASE_URL_AGE = "https://api.agify.io"
+    private const val BASE_URL_GENDER = "https://api.genderize.io"
+    private const val BASE_URL_NATIONALITY = "https://api.nationalize.io"
 
     @Provides
     @Singleton
-    fun provideNetworkService(
-        baseUrl: String,
-        gsonFactory: GsonConverterFactory,
-    ): NetworkService {
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
 
+    @Provides
+    @Singleton
+    fun provideRetrofitForAge(
+        gsonFactory: GsonConverterFactory,
+    ): NetworkServiceForAge {
         return Retrofit.Builder()
-            .baseUrl(baseUrl)
+            .baseUrl(BASE_URL_AGE)
             .addConverterFactory(gsonFactory)
             .build()
-            .create(NetworkService::class.java)
+            .create(NetworkServiceForAge::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitForGender(
+        gsonFactory: GsonConverterFactory,
+    ): NetworkServiceForGender {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL_GENDER)
+            .addConverterFactory(gsonFactory)
+            .build()
+            .create(NetworkServiceForGender::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitForNationality(
+        gsonFactory: GsonConverterFactory,
+    ): NetworkServiceForNationality {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL_NATIONALITY)
+            .addConverterFactory(gsonFactory)
+            .build()
+            .create(NetworkServiceForNationality::class.java)
     }
 
     @Provides
