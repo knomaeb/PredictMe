36f8e9faaa5c5a6b29ac591abd254e51bb24ae79
Sunil Rana <sunil.rana320778@gmail.com>
2025-06-14 10:24:28 +0530

    feat: Model Person as three separate classes

--- app/src/main/java/com/example/predictme/model/Person.kt
+++ app/src/main/java/com/example/predictme/model/Person.kt
@@ -1,8 +1,59 @@
 package com.example.predictme.model
 
-data class Person(
+import com.google.gson.annotations.SerializedName
+
+data class PersonAge(
+
+    @SerializedName("count")
+    val count: Int,
+
+    @SerializedName("name")
     val name : String,
+
+    @SerializedName("age")
     val age: String,
+
+)
+
+data class PersonGender(
+
+    @SerializedName("count")
+    val count: Int,
+
+    @SerializedName("name")
+    val name : String,
+
+    @SerializedName("gender")
     val gender: String,
-    val nationality: String
+
+    @SerializedName("probability")
+    val probability : Long
+
+)
+
+data class PersonNationality(
+
+    @SerializedName("count")
+    val count: Int,
+
+    @SerializedName("name")
+    val name : String,
+
+    @SerializedName("gender")
+    val country : List<Country>
+
+)
+
+data class Country(
+    @SerializedName("country_id")
+    val country_id : String,
+
+    @SerializedName("probability")
+    val probability : Long
+)
+
+data class CombinedData(
+    val PersonAge : PersonAge,
+    val PersonGender : PersonGender,
+    val PersonNationality : PersonNationality
 )
